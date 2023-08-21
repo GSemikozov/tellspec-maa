4a3f49f8e9f35cbc953ef9b52b597223f1f87ce9
Herman Semykozov <semikozovgerman@gmail.com>
2023-08-21 06:55:53 +0300

    update login page layout

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,13 +1,9 @@
 import React, { useEffect } from "react";
-import { Route } from 'react-router-dom';
+import { Route } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
-import {
-  IonApp,
-  IonRouterOutlet,
-  setupIonicReact
-} from '@ionic/react';
-import { IonReactRouter } from '@ionic/react-router';
+import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
+import { IonReactRouter } from "@ionic/react-router";
 import { ProtectedRoute } from "./components/protected-route";
 
 import { fetchAppSettings } from "./model/app.actions";
@@ -17,7 +13,8 @@ import { HomePage } from "../pages/home";
 
 import type { AppDispatch } from "./store";
 
-import './app.css';
+import "./app.css";
+import { AddMilkPage } from "../pages/add-milk";
 
 setupIonicReact();
 
@@ -39,8 +36,11 @@ export const App: React.FC = () => {
           <ProtectedRoute exact path="/">
             <HomePage />
           </ProtectedRoute>
+          <ProtectedRoute exact path="/add-milk">
+            <AddMilkPage />
+          </ProtectedRoute>
         </IonRouterOutlet>
       </IonReactRouter>
     </IonApp>
   );
-}
+};
--- src/pages/login/login.page.tsx
+++ src/pages/login/login.page.tsx
@@ -1,24 +1,38 @@
 import React from "react";
 import {
+  IonCol,
   IonContent,
+  IonGrid,
   IonHeader,
   IonPage,
+  IonRow,
   IonTitle,
-  IonToolbar
+  IonToolbar,
 } from "@ionic/react";
 import { LoginForm } from "../../features/login-form/login-form";
+import { Logo } from "../../app/components/logo";
 
 export const LoginPage: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Login</IonTitle>
-        </IonToolbar>
-      </IonHeader>
       <IonContent className="ion-padding">
-        <LoginForm />
+        <IonGrid style={{ height: "100%" }}>
+          <div style={{ display: "flex", height: "100%" }}>
+            <IonCol size="8" style={{ background: "grey" }} />
+            <IonCol
+              size="4"
+              style={{ display: "flex", flexDirection: "column" }}
+              className="ion-justify-content-center ion-align-items-center"
+            >
+              <div style={{ maxWidth: "70%" }}>
+                <Logo />
+                <div className="ion-margin-top ion-margin-bottom" />
+                <LoginForm />
+              </div>
+            </IonCol>
+          </div>
+        </IonGrid>
       </IonContent>
     </IonPage>
-  )
-}
+  );
+};
