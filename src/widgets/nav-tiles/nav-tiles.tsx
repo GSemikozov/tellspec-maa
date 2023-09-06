0eeb3b3a3f63ada08c10e839028e81dc618d36bb
Maggie Recio <m.reciogrom@gmail.com>
2023-09-06 11:50:01 +0100

    changed font, home page tiles UI

--- src/widgets/nav-tiles/nav-tiles.tsx
+++ src/widgets/nav-tiles/nav-tiles.tsx
@@ -1,22 +1,27 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { IonText } from '@ionic/react';
-import { routesMapping } from '../../app/routes';
+import React from "react";
+import { Link } from "react-router-dom";
+import { IonText } from "@ionic/react";
+import { routesMapping } from "../../app/routes";
 
-import './nav-tiles.css'
+import "./nav-tiles.css";
 
 export const NavTiles: React.FC = () => {
   return (
     <>
       <Link className="tab" to={routesMapping.addMilk}>
-        <div className="button-tab ion-float-left">
+        <img
+          src="../../assets/images/logo-tile-top.svg"
+          alt="top part of logo"
+          className="logo-tile-top"
+        />
+        <div className="button-tab ion-float-right">
           <img
             className="icons"
             src="../../../assets/images/add-milk-selected.png"
             alt="add milk icon"
           />
         </div>
-        <IonText className="ion-text-end ">
+        <IonText className="ion-text-start">
           <h4>
             Start By Scanning <br />
             The Barcode Or
@@ -24,20 +29,25 @@ export const NavTiles: React.FC = () => {
             Entering The Milk ID
           </h4>
         </IonText>
-        <IonText className="ion-text-end">
+        <IonText className="ion-text-start">
           <h3>Add Milk</h3>
         </IonText>
       </Link>
 
       <Link className="tab" to={routesMapping.analyse}>
-        <div className="button-tab ion-float-left">
+        <img
+          src="../../assets/images/logo-tile-middle.svg"
+          alt="top part of logo"
+          className="logo-tile-middle"
+        />
+        <div className="button-tab ion-float-right">
           <img
             className="icons"
             src="../../../assets/images/analyse-milk-selected.png"
             alt="analyse milk icon"
           />
         </div>
-        <IonText className="ion-text-end ">
+        <IonText className="ion-text-start">
           <h4>
             Use PREEMIE SENSOR
             <br />
@@ -46,20 +56,25 @@ export const NavTiles: React.FC = () => {
             Selected Milk
           </h4>
         </IonText>
-        <IonText className="ion-text-end">
+        <IonText className="ion-text-start">
           <h3>Analyse Milk</h3>
         </IonText>
       </Link>
 
       <Link className="tab" to={routesMapping.reports}>
-        <div className="button-tab ion-float-left">
+        <img
+          src="../../assets/images/logo-tile-bottom.svg"
+          alt="top part of logo"
+          className="logo-tile-bottom"
+        />
+        <div className="button-tab ion-float-right">
           <img
             className="icons"
             src="../../../assets/images/view-reports-selected.png"
             alt="view reports icon"
           />
         </div>
-        <IonText className="ion-text-end">
+        <IonText className="ion-text-start">
           <h4>
             PrintBags Labels
             <br />
@@ -68,7 +83,7 @@ export const NavTiles: React.FC = () => {
             Milk Reports
           </h4>
         </IonText>
-        <IonText className="ion-text-end">
+        <IonText className="ion-text-start">
           <h3>View Reports</h3>
         </IonText>
       </Link>
