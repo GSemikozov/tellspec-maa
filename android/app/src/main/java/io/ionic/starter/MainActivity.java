8f5d8fd0dd7e63c9eea5878d589df4aaef32bc2b
Herman Semykozov <semikozovgerman@gmail.com>
2023-09-08 14:38:10 +0300

    wip: update android configs (maybe remove in future)

--- android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -2,11 +2,4 @@ package io.ionic.starter;
 
 import com.getcapacitor.BridgeActivity;
 
-// public class MainActivity extends BridgeActivity {}
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        registerPlugin(BarcodePrinter.class);
-        super.onCreate(savedInstanceState);
-    }
-}
+public class MainActivity extends BridgeActivity {}
