8bd0788aa47e60d80c0dffa75802b0e004656055
Herman Semykozov <semikozovgerman@gmail.com>
2023-09-11 01:30:39 +0300

    rewrap add-milk

--- src/pages/add-milk/add-milk.page.tsx
+++ src/pages/add-milk/add-milk.page.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
+import { IonContent, IonPage } from '@ionic/react';
 
 import { AddMilkForm } from '@features/add-milk-form';
 import { Layout } from '@widgets/layout/layout';
 
 export const AddMilkPage: React.FC = () => {
     return (
-        <Layout>
-            {/* <IonGrid>
-          <IonRow>
-            <IonCol size='2.5'>
-            <SidebarMenu />
-            </IonCol>
-            <IonCol size='9.5'>
-
-            </IonCol>
-            </IonRow>
-          </IonGrid> */}
-            <AddMilkForm />
-        </Layout>
+        <IonPage>
+            <IonContent>
+                <Layout>
+                    <AddMilkForm />
+                </Layout>
+            </IonContent>
+        </IonPage>
     );
 };
