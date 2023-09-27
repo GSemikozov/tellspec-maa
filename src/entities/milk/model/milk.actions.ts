5e6b1fcc70e9bfd20c18741e6cee356fab13d17d
Herman Semykozov <semikozovgerman@gmail.com>
2023-09-27 11:48:05 +0300

    remove unnecessary param

--- src/entities/milk/model/milk.actions.ts
+++ src/entities/milk/model/milk.actions.ts
@@ -3,7 +3,6 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 import { RootState } from '@app';
 import { apiInstance } from '@api/network';
 import { tellspecRunScan } from '@api/native';
-import { GetCalibrationResponse } from '@entities/sensor';
 
 export const fetchMilks = createAsyncThunk('milks/fetch-list', async () => {
     try {
@@ -23,5 +22,5 @@ export const fetchMilks = createAsyncThunk('milks/fetch-list', async () => {
 export const analyseMilk = createAsyncThunk('milks/analyse-milk', async (_, thunkAPI) => {
     const { user } = thunkAPI.getState() as RootState;
 
-    return tellspecRunScan(user.email, {} as GetCalibrationResponse);
+    return tellspecRunScan(user.email);
 });
