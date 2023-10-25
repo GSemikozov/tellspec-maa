3b6c6823cfcbbdb577b985daba65727e6b5917be
Herman Semykozov <herman@tellspec.com>
2023-10-25 14:05:28 +0300

    feat: add email to server log

--- src/entities/common/api/common.api.ts
+++ src/entities/common/api/common.api.ts
@@ -1,4 +1,5 @@
 import { BaseEndpoint } from '@api/network';
+import { getUserLocalData } from '@entities/user/user.utils';
 
 import type { AddLogRequest, AddLogResponse } from './types';
 
@@ -6,10 +7,13 @@ export class CommonApi extends BaseEndpoint {
     private addLogUrl = '/logs/logApps/';
 
     addLog = async (request: Omit<AddLogRequest, 'app' | 'version_store' | 'date'>) => {
+        const user = await getUserLocalData();
+
         const requestBody: AddLogRequest = {
             ...request,
             app: 'Preemie Milk Analysis App',
             version_store: '1',
+            email: user?.email ?? '',
         };
 
         const response = await this.http.post<AddLogResponse>(this.addLogUrl, requestBody);
